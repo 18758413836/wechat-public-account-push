390c079528d25d37b956ee74e37d63fbea9b324d
18758413836 <112537196+18758413836@users.noreply.github.com>
2022-08-31 13:51:56 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -4,37 +4,17 @@ export const config = {
      */
 
     // 公众号APP_ID
-    APP_ID: "",
+    APP_ID: "wx444d6d2b8fec87a5",
 
     // 公众号APP_SECRET
-    APP_SECRET: "",
+    APP_SECRET: "3dc8a48668ddb96a58b303eb03f335ab",
 
     // 模板消息id
-    TEMPLATE_ID: "xxxxxxxxxxxxx-y你懂的,填错狗头打烂VRMpu",
-
-    // 回调消息模板id, 用来看自己有没有发送成功的那个模板
-    CALLBACK_TEMPLATE_ID: "xxxxxxxxxxxxx-y你懂的,填错狗头打烂VRMpu",
-
-    // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如
-    // [{
-    //   name: "张三",
-    //   id: "wxafasdasdd-zxfvsdfd"
-    // },
-    // {
-    //   name: "李四",
-    //   id: "wxafasdasdd-zxfvsdfd"
-    // }]
-    USERS: [
-      {name: '老婆0', id: "1hxOO6VskYv0Kuf-y你懂的,填错狗头打烂VRMpuXhk"}, 
-      {name: '老婆1', id: "2hxOO6VskYv0Kuf-y你懂的,填错狗头打烂RMpuXhk"}, 
-      {name: '老婆2', id: "3hxOO6VskYv0Kuf-yw你懂的,填错狗头打烂2RMpuXhk"}, 
-      {name: '老婆3', id: "4hxOO6VskYv0Kuf-ywI你懂的,填错狗头打烂MpuXhk"}, 
-    ],
-    // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
-    CALLBACK_USERS: [
-      {name: '自己',id: "5hxOO6VskYv你懂的,填错狗头打烂-ywIZVRMpuXhk"}, 
-    ],
-     
+    TEMPLATE_ID: "7HfnhHdjuYl6W9uYACrkdAC6zs0aFN09pkATs2-ALMs",  
+    
+    // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1","wx2"]
+    USERS: ["oYN-758D1YL6PZOv3-IuKL5bJNPk"],
+    /
     /**
      * 信息配置
      */
@@ -42,7 +22,7 @@ export const config = {
     /** 天气相关 */
 
     // 所在省份
-    PROVINCE: "广东",
+    PROVINCE: "上海",
     // 所在城市
     CITY: "惠州",
 
@@ -53,10 +33,10 @@ export const config = {
     // 生日时，name填写想要展示的名字，你可以填“美丽可爱亲亲老婆”
     // 节日时，name填写相应展示的节日，你可以填“被搭讪纪念日”
     FESTIVALS: [
-      {"type": "生日", "name": "老婆", "year": "1996", "date": "01-01"},
-      {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "01-04"},
-      {"type": "生日", "name": "李四", "year": "1996", "date": "08-29"},
-      {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "08-29"},
+      {"type": "生日", "name": "景景", "year": "2001", "date": "08-20"},
+      {"type": "节日", "name": "在一起", "year": "2022", "date": "08-19"},
+      {"type": "生日", "name": "欣欣", "year": "2003", "date": "04-03"},
+      {"type": "节日", "name": "见面的日子", "year": "2023", "date": "06-29"},
     ],
     // 限制重要节日的展示条目, 需要填写数字; 
     // 如果为3, 则仅展示“将要到达” 的3个重要节日提醒，剩下的将被忽略
@@ -66,9 +46,9 @@ export const config = {
     /** 日期相关 */
 
     // 在一起的日子，格式同上
-    LOVE_DATE: "2015-05-01",
-    // 结婚纪念日
-    MARRY_DATE: "2020-01-04",
+    LOVE_DATE: "2022-08-19",
+    // 我爱景
+    MARRY_DATE: "2022-08-19",
 
 
     /** 每日一言 */
